dcc69678e96ea0eb0be2fbc9e1b4c64803a19ba7
IngDavidTM <david-1011@hotmail.com>
2025-09-23 09:18:44 -0500

    Change burguer menu

--- src/modules/Menu.js
+++ src/modules/Menu.js
@@ -1,24 +1,97 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBurger } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 import '../stylesheets/Menu.css';
 
-const Menu = (menu) => {
-  const { scrollToSectionMobile, setMenu } = menu;
+const Menu = ({ scrollToSectionMobile, setMenu }) => {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      setMenu(false);
+    }
+  };
+
   return (
-    <nav className="nav_menu">
-      <div className="title_menu">
-        <button type="button" aria-label="close" className="button" onClick={() => { setMenu(!menu); }}>
-          <FontAwesomeIcon icon={faBurger} className="nav_icon" />
-        </button>
-        <h3>Burger Menu</h3>
-      </div>
-      <ul className="nav_list_menu">
-        <li><button type="button" onClick={() => scrollToSectionMobile('works', 2)}>Works</button></li>
-        <li><button type="button" onClick={() => scrollToSectionMobile('works', 3)}>About</button></li>
-        <li><button type="button" onClick={() => scrollToSectionMobile('works', 4)}>Contact</button></li>
-      </ul>
-    </nav>
+    <>
+      {/* Overlay con glassmorphism */}
+      <div
+        className="menu_overlay"
+        onClick={() => setMenu(false)}
+        onKeyDown={handleKeyDown}
+        role="button"
+        tabIndex={0}
+        aria-label="Close menu"
+      />
+
+      {/* Menú con diseño elegante */}
+      <nav className="nav_menu">
+        <div className="menu_header">
+          <div className="menu_brand">
+            <h2>Navigation</h2>
+            <p>Explore my portfolio</p>
+          </div>
+          <button
+            type="button"
+            aria-label="close menu"
+            className="close_btn"
+            onClick={() => setMenu(false)}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+
+        <ul className="nav_list_menu">
+          <li className="nav_item">
+            <button
+              type="button"
+              className="nav_link"
+              onClick={() => {
+                scrollToSectionMobile('works', 2);
+                setMenu(false);
+              }}
+            >
+              <span className="nav_number">01</span>
+              <span className="nav_text">Works</span>
+            </button>
+          </li>
+          <li className="nav_item">
+            <button
+              type="button"
+              className="nav_link"
+              onClick={() => {
+                scrollToSectionMobile('about', 3);
+                setMenu(false);
+              }}
+            >
+              <span className="nav_number">02</span>
+              <span className="nav_text">About</span>
+            </button>
+          </li>
+          <li className="nav_item">
+            <button
+              type="button"
+              className="nav_link"
+              onClick={() => {
+                scrollToSectionMobile('contact', 4);
+                setMenu(false);
+              }}
+            >
+              <span className="nav_number">03</span>
+              <span className="nav_text">Contact</span>
+            </button>
+          </li>
+        </ul>
+
+        <div className="menu_footer">
+          <p>Ready to collaborate?</p>
+        </div>
+      </nav>
+    </>
   );
+};
+
+Menu.propTypes = {
+  scrollToSectionMobile: PropTypes.func.isRequired,
+  setMenu: PropTypes.func.isRequired,
 };
 
 export default Menu;
